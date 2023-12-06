ce96efbf30db557e63c18f48d1eb3d88e7092481
Wasif Saeed <115382944+Wasif0410@users.noreply.github.com>
2023-12-05 23:31:28 -0500

    Update Index.py

--- Index.py
+++ Index.py
@@ -1,13 +1,33 @@
 # app.py
 import streamlit as st
+import random
 
 def main():
-    st.title("Simple Streamlit Page with X's")
+    st.title("CPS530 LAB 10")
+    st.subheader("Streamlit Framework")
 
-    num_paragraphs = st.slider("Number of Paragraphs:", min_value=1, max_value=10, value=5)
+    # Box 1
+    st.header("Box 1")
+    with st.expander("Click to show random text"):
+        st.write(get_random_text())
 
-    for _ in range(num_paragraphs):
-        st.write("X" * 100)
+    # Box 2
+    st.header("Box 2")
+    with st.expander("Click to show random text"):
+        st.write(get_random_text())
+
+    # Box 3
+    st.header("Box 3")
+    with st.expander("Click to show random text"):
+        st.write(get_random_text())
+
+def get_random_text():
+    # Generating random text
+    paragraphs = ["Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                  "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+                  "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris.",
+                  "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."]
+    return random.choice(paragraphs)
 
 if __name__ == "__main__":
     main()
